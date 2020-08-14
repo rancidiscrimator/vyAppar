5b2c8046171f3485e238357a8e5f14275427c41b
Alankrit Bhardwaj <48072177+alan478@users.noreply.github.com>
2020-08-14 17:25:47 +0530

    Social media adapter and classes created

---
+++ app/src/main/java/com/example/gov/Adapter/PostAdapter.java
@@ -0,0 +1,78 @@
+package com.example.gov.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.gov.ModalClasses.ChatContactModalClass;
+import com.example.gov.ModalClasses.PostModalClasses;
+import com.example.gov.R;
+
+import java.util.List;
+
+
+
+
+    public class PostAdapter extends RecyclerView.Adapter<com.example.gov.Adapter.PostAdapter.PostviewHolder> {
+
+        private Context mct;
+        private List<PostModalClasses> list;
+
+        public PostAdapter(Context mct, List<PostModalClasses> list) {
+            this.mct = mct;
+            this.list = list;
+        }
+
+        @Override
+        public com.example.gov.Adapter.PostAdapter.PostviewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(mct);
+            View view = inflater.inflate(R.layout.post_layout, null);
+            return new com.example.gov.Adapter.PostAdapter.PostviewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PostviewHolder holder, int position) {
+            PostModalClasses classes = list.get(position);
+            holder.tv1.setText(classes.getDescription());
+            
+
+
+
+        }
+
+
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        public class PostviewHolder extends RecyclerView.ViewHolder {
+
+            TextView tv1;
+            ImageView image;
+
+
+            public PostviewHolder(View itemView) {
+                super(itemView);
+
+                tv1 = itemView.findViewById(R.id.tv1);
+                image=itemView.findViewById(R.id.image);
+
+
+
+
+            }
+
+
+        }
+
+
+    }
+
--- app/src/main/java/com/example/gov/ModalClasses/PostModalClasses.java
+++ app/src/main/java/com/example/gov/ModalClasses/PostModalClasses.java
@@ -2,6 +2,27 @@ package com.example.gov.ModalClasses;
 
 public class PostModalClasses {
 
-    
+    String description;
+    String url;
 
+    public PostModalClasses(String description, String url) {
+        this.description = description;
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
