897f42a683500cea2785dc94559b4a15302be204
Alankrit Bhardwaj <48072177+alan478@users.noreply.github.com>
2020-08-17 16:21:08 +0530

    initial commit

---
+++ app/src/main/java/com/example/gov/Activity/VendorProfileCustomerSide.java
@@ -0,0 +1,15 @@
+package com.example.gov.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gov.R;
+
+public class VendorProfileCustomerSide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_profile_customer_side);
+    }
+}
