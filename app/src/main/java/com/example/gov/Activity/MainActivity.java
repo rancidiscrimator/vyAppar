d160c37e4047be24fdd88fa77581ab3daf0cc415
Alankrit Bhardwaj <48072177+alan478@users.noreply.github.com>
2020-08-17 12:32:55 +0530

    changes in database to made for better handlling while switching from customer to vendor

--- app/src/main/java/com/example/gov/Activity/MainActivity.java
+++ app/src/main/java/com/example/gov/Activity/MainActivity.java
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
 
                             }
                         });
-                    }else
+                    }else         
                     {
 
 
