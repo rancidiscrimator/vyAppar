073dd87e707332f95d293c78c6fd9ecd8ddd651c
Alankrit Bhardwaj <48072177+alan478@users.noreply.github.com>
2020-08-18 17:38:14 +0530

    shifting from customer to vendor part done

--- app/src/main/java/com/example/gov/Fragment/Fragment_Settings_Preference.java
+++ app/src/main/java/com/example/gov/Fragment/Fragment_Settings_Preference.java
@@ -3,21 +3,32 @@ package com.example.gov.Fragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
 
+import com.example.gov.Activity.LoginScreen;
+import com.example.gov.Activity.MainActivity;
 import com.example.gov.Activity.RegisterActivity;
+import com.example.gov.Activity.VendorAuth;
 import com.example.gov.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class Fragment_Settings_Preference extends PreferenceFragmentCompat {
     Toolbar toolbar;
+    FirebaseAuth auth;
+    FirebaseUser user;
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(com.example.gov.R.xml.shared_preferences);
@@ -27,16 +38,63 @@ public class Fragment_Settings_Preference extends PreferenceFragmentCompat {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        auth=FirebaseAuth.getInstance();
+        user=auth.getCurrentUser();
         SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(getContext());
         findPreference("switch_preference_1").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 FirebaseAuth firebaseAuth= FirebaseAuth.getInstance();
                 firebaseAuth.signOut();
-                Intent intent=new Intent(getContext(), RegisterActivity.class);
+                Intent intent=new Intent(getContext(), LoginScreen.class);
                 startActivity(intent);
                 return false;
             }
         });
+
+        findPreference("vendor").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                FirebaseFirestore firestore=FirebaseFirestore.getInstance();
+                firestore.collection("Vendor").document(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if(task.isSuccessful())
+                        {
+                            DocumentSnapshot documentSnapshot=task.getResult();
+
+                            if(documentSnapshot.exists())
+                            {
+                                Intent intent=new Intent(getContext(), VendorAuth.class);
+                                intent.putExtra("value","1");
+                                startActivity(intent);
+                            }
+                            else
+                            {
+
+                                Intent intent=new Intent(getContext(), RegisterActivity.class);
+                                Toast.makeText(getContext(),"Register Yourself as vendor First",Toast.LENGTH_LONG).show();
+                                startActivity(intent);
+                            }
+
+
+                        }
+
+
+                    }
+                });
+
+
+
+
+                return false;
+            }
+        });
+
+
+
+
     }
 }
